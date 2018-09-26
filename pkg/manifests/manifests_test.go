6e063152dd948cddea9aaad4da6d9c3a102d3fa7
Dan Mace <ironcladlou@gmail.com>
2018-09-26 15:03:20 -0400

    Expose operator manifests through factory
    Incorporate the operator manifests into code generation and expose them through
    the factory to support direct integration with the new OpenShift installer.

--- pkg/manifests/manifests_test.go
+++ pkg/manifests/manifests_test.go
@@ -16,6 +16,24 @@ func TestManifests(t *testing.T) {
 		},
 	}
 
+	if _, err := f.OperatorNamespace(); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := f.OperatorCustomResourceDefinition(); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := f.OperatorClusterRole(); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := f.OperatorClusterRoleBinding(); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := f.OperatorDeployment(); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := f.OperatorServiceAccount(); err != nil {
+		t.Fatal(err)
+	}
 	if _, err := f.DNSNamespace(); err != nil {
 		t.Errorf("invalid DNSNamespace: %v", err)
 	}
