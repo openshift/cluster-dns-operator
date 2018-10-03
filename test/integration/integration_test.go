2fc62a2405ab73c178ee376b099e2a3ac8b50d5a
Ravi Sankar Penta <rpenta@redhat.com>
2018-10-03 10:41:44 -0700

    Change integration tests to comply with new changes

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -14,12 +14,11 @@ import (
 	"time"
 
 	dnsv1alpha1 "github.com/openshift/cluster-dns-operator/pkg/apis/dns/v1alpha1"
+	"github.com/openshift/cluster-dns-operator/pkg/manifests"
 	stub "github.com/openshift/cluster-dns-operator/pkg/stub"
-	"github.com/openshift/cluster-dns-operator/test/manifests"
 
 	kubeclient "github.com/operator-framework/operator-sdk/pkg/k8sclient"
 	sdk "github.com/operator-framework/operator-sdk/pkg/sdk"
-	k8sutil "github.com/operator-framework/operator-sdk/pkg/util/k8sutil"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -27,13 +26,21 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 )
 
+const (
+	DNSNamespace   = "openshift-cluster-dns"
+	AppNamespace   = "cluster-dns-test"
+	AppServiceName = "app"
+
+	ClusterDNSCustomResourceName = "test-default"
+)
+
 var testConfig *TestConfig
 
 func TestIntegration(t *testing.T) {
 	testConfig = NewTestConfig(t)
 
-	createCRD()
-	defer deleteCRD()
+	createManifests()
+	defer deleteManifests()
 
 	startOperator()
 
@@ -42,69 +49,16 @@ func TestIntegration(t *testing.T) {
 }
 
 func testDNSRecords(t *testing.T) {
+	createAppManifests()
+	defer deleteAppManifests()
+
+	clusterDNS, err := getClusterDNSCustomResource()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	f := manifests.NewFactory()
-
-	appNamespace, err := f.AppDNSNamespace()
-	if err != nil {
-		t.Fatal(err)
-	}
-	appDeployment, err := f.AppDNSDeployment()
-	if err != nil {
-		t.Fatal(err)
-	}
-	appService, err := f.AppDNSService()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	dnsNamespace, err := f.DNSNamespace()
-	if err != nil {
-		t.Fatal(err)
-	}
-	clusterDNS, err := f.ClusterDNSCustomResource()
-	if err != nil {
-		t.Fatal(err)
-	}
-	clusterDNS.Namespace = testConfig.operatorNamespace
-
 	dnsService, err := f.DNSService(clusterDNS)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	cleanup := func() {
-		leftovers := []sdk.Object{
-			clusterDNS,
-			dnsNamespace,
-			appNamespace,
-		}
-		anyFailed := false
-		for _, o := range leftovers {
-			err := sdk.Delete(o)
-			if err != nil && !errors.IsNotFound(err) {
-				t.Logf("failed to clean up object %#v: %s", o, err)
-				anyFailed = true
-			}
-		}
-		if anyFailed {
-			t.Fatalf("failed to clean up resources")
-		}
-	}
-	defer cleanup()
-
-	err = sdk.Create(appNamespace)
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = sdk.Create(appDeployment)
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = sdk.Create(appService)
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = sdk.Create(clusterDNS)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -116,7 +70,7 @@ func testDNSRecords(t *testing.T) {
 	}
 	t.Logf("configured cluster domain: %s, clusterIP: %s:%s", clusterDomain, serviceIP, servicePort)
 
-	testPod, err := getDNSPod(dnsNamespace.Name)
+	testPod, err := getDNSPod(DNSNamespace)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -135,7 +89,7 @@ func testDNSRecords(t *testing.T) {
 			dnsType: "a",
 		},
 		{
-			domain:  fmt.Sprintf("%s.%s.svc.%s.", appService.Name, appService.Namespace, clusterDomain),
+			domain:  fmt.Sprintf("%s.%s.svc.%s.", AppServiceName, AppNamespace, clusterDomain),
 			dnsType: "a",
 		},
 		{
@@ -143,14 +97,14 @@ func testDNSRecords(t *testing.T) {
 			dnsType: "srv",
 		},
 		{
-			domain:  fmt.Sprintf("_http._tcp.%s.%s.svc.%s.", appService.Name, appService.Namespace, clusterDomain),
+			domain:  fmt.Sprintf("_http._tcp.%s.%s.svc.%s.", AppServiceName, AppNamespace, clusterDomain),
 			dnsType: "srv",
 		},
 	}
 
 	for _, test := range tests {
 		digCmd := fmt.Sprintf("dig +noall +answer %s %s @%s -p %s", test.dnsType, test.domain, serviceIP, servicePort)
-		cmd := fmt.Sprintf("oc exec %s -n %s -- %s", testPod, dnsNamespace.Name, digCmd)
+		cmd := fmt.Sprintf("oc exec %s -n %s -- %s", testPod, DNSNamespace, digCmd)
 		msg := fmt.Sprintf("test domain: %s, type: %s", test.domain, test.dnsType)
 
 		outStr, errStr := runShellCmd(cmd, msg)
@@ -160,6 +114,23 @@ func testDNSRecords(t *testing.T) {
 			t.Fatalf("failed to %s, invalid answer", msg)
 		}
 	}
+}
+
+func getClusterDNSCustomResource() (*dnsv1alpha1.ClusterDNS, error) {
+	cr := &dnsv1alpha1.ClusterDNS{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ClusterDNS",
+			APIVersion: "dns.openshift.io/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      ClusterDNSCustomResourceName,
+			Namespace: testConfig.operatorNamespace,
+		},
+	}
+	if err := sdk.Get(cr); err != nil {
+		return nil, err
+	}
+	return cr, nil
 }
 
 func getClusterDomain(dns *dnsv1alpha1.ClusterDNS) string {
@@ -246,12 +217,8 @@ func NewTestConfig(t *testing.T) *TestConfig {
 	os.Setenv("KUBERNETES_CONFIG", kubeConfig)
 	config.kubeConfig = kubeConfig
 
-	namespace, err := k8sutil.GetWatchNamespace()
-	if err != nil {
-		namespace = "default"
-		os.Setenv("WATCH_NAMESPACE", namespace)
-	}
-	config.operatorNamespace = namespace
+	config.operatorNamespace = "default"
+	os.Setenv("WATCH_NAMESPACE", config.operatorNamespace)
 
 	// NOTE: Do not call sdk.GetKubeClient() or sdk.GetKubeConfig() in this method.
 	// Calling these methods before starting operator will intialize the sdk rest mapper
@@ -271,12 +238,29 @@ func startOperator() {
 	go sdk.Run(context.TODO())
 }
 
-func createCRD() {
-	runShellCmd(fmt.Sprintf("oc apply -f ../../manifests/0000_08_cluster-dns-operator_00-custom-resource-definition.yaml -n %s", testConfig.operatorNamespace), "create cluster dns CRD")
+func createAppManifests() {
+	runShellCmd("oc apply -f manifests/", "create app manifests")
 }
 
-func deleteCRD() {
+func deleteAppManifests() {
+	runShellCmd(fmt.Sprintf("oc delete ns %s", AppNamespace), "delete app namespace")
+	runShellCmd(fmt.Sprintf("oc delete clusterdns %s -n %s", ClusterDNSCustomResourceName, testConfig.operatorNamespace), "delete cluster dns custom resource")
+}
+
+func createManifests() {
+	runShellCmd(fmt.Sprintf("oc apply -f ../../manifests/0000_08_cluster-dns-operator_00-custom-resource-definition.yaml -n %s", testConfig.operatorNamespace), "create cluster dns CRD")
+	runShellCmd("oc apply -f ../../manifests/0000_08_cluster-dns-operator_00-dns-namespace.yaml", "create dns namespace")
+	runShellCmd("oc apply -f ../../manifests/0000_08_cluster-dns-operator_00-dns-cluster-role.yaml", "create dns cluster role")
+	runShellCmd("oc apply -f ../../manifests/0000_08_cluster-dns-operator_01-dns-cluster-role-binding.yaml", "create dns cluster role binding")
+	runShellCmd("oc apply -f ../../manifests/0000_08_cluster-dns-operator_01-dns-service-account.yaml", "create dns service account")
+}
+
+func deleteManifests() {
 	runShellCmd(fmt.Sprintf("oc delete crd clusterdnses.dns.openshift.io -n %s", testConfig.operatorNamespace), "delete cluster dns CRD")
+	runShellCmd(fmt.Sprintf("oc delete ns %s", DNSNamespace), "delete dns namespace")
+	runShellCmd(fmt.Sprintf("oc delete clusterrolebinding cluster-dns:dns -n %s", DNSNamespace), "delete dns cluster role binding")
+	runShellCmd(fmt.Sprintf("oc delete clusterrole cluster-dns:dns -n %s", DNSNamespace), "delete dns cluster role")
+	runShellCmd(fmt.Sprintf("oc delete sa dns -n %s", DNSNamespace), "delete dns service account")
 }
 
 func runShellCmd(command, msg string) (string, string) {
