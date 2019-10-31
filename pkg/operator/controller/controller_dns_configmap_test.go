86205d196bac58ff79f5b933ee2800b5e83c6be2
Miciah Masters <miciah.masters@gmail.com>
2019-10-31 02:26:09 -0400

    Implement DNS forwarding API
    * pkg/manifests/manifests.go (DNSConfigMapAsset): Delete constant.
    (DNSConfigMap): Delete function.
    * pkg/manifests/manifests_test.go (TestManifests): Delete DNSConfigMap.
    * pkg/operator/controller/controller_dns_configmap.go (corefileTemplate): New
    variable containing a Corefile template.
    (corefileTemplateFunctions): New template function map.
    (translatePolicyName): New function.  Translate DNS policy from the API name to
    the Corefile string.  Used in corefileTemplateFunctions.
    (ensureDNSConfigMap): Handle errors from desiredDNSConfigMap.
    (desiredDNSConfigMap): Add error return value.  Replace DNSConfigMap from the
    manifests package by using corefileTemplate and corefileTemplateFunctions to
    generate a Corefile from the parameters in the given DNS.
    * pkg/operator/controller/controller_dns_configmap_test.go: New file.
    (TestDesiredDNSConfigmap): New test.

---
+++ pkg/operator/controller/controller_dns_configmap_test.go
@@ -0,0 +1,65 @@
+package controller
+
+import (
+	"testing"
+
+	operatorv1 "github.com/openshift/api/operator/v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestDesiredDNSConfigmap(t *testing.T) {
+	clusterDomain := "cluster.local"
+	dns := &operatorv1.DNS{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: DefaultDNSController,
+		},
+		Spec: operatorv1.DNSSpec{
+			Servers: []operatorv1.Server{
+				{
+					Name:  "foo",
+					Zones: []string{"foo.com"},
+					ForwardPlugin: operatorv1.ForwardPlugin{
+						Upstreams: []string{"1.1.1.1", "2.2.2.2:5353"},
+					},
+				},
+				{
+					Name:  "bar",
+					Zones: []string{"bar.com", "example.com"},
+					ForwardPlugin: operatorv1.ForwardPlugin{
+						Upstreams: []string{"3.3.3.3"},
+					},
+				},
+			},
+		},
+	}
+	expectedCorefile := `# foo
+foo.com:5353 {
+    forward . 1.1.1.1 2.2.2.2:5353
+}
+# bar
+bar.com:5353 example.com:5353 {
+    forward . 3.3.3.3
+}
+.:5353 {
+    errors
+    health
+    kubernetes cluster.local in-addr.arpa ip6.arpa {
+        pods insecure
+        upstream
+        fallthrough in-addr.arpa ip6.arpa
+    }
+    prometheus :9153
+    forward . /etc/resolv.conf {
+        policy sequential
+    }
+    cache 30
+    reload
+}
+`
+	if cm, err := desiredDNSConfigMap(dns, clusterDomain); err != nil {
+		t.Errorf("invalid dns configmap: %v", err)
+	} else if cm.Data["Corefile"] != expectedCorefile {
+		t.Errorf("unexpected Corefile; got:\n%s\nexpected:\n%s\n", cm.Data["Corefile"], expectedCorefile)
+	}
+}
