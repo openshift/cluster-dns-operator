dfc59fcf019114c8a15e2558136034b3c2980226
Chad Scribner <chad@redhat.com>
2022-06-16 14:52:11 -0400

    Improve desiredCABundleConfigMap test coverage

--- pkg/operator/controller/controller_cabundle_configmap_test.go
+++ pkg/operator/controller/controller_cabundle_configmap_test.go
@@ -1,13 +1,12 @@
 package controller
 
 import (
-	"testing"
-
 	"github.com/google/go-cmp/cmp"
 	operatorv1 "github.com/openshift/api/operator/v1"
-
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"testing"
+	"time"
 )
 
 func TestDesiredCABundleConfigmap(t *testing.T) {
@@ -28,12 +27,28 @@ func TestDesiredCABundleConfigmap(t *testing.T) {
 		Spec: operatorv1.DNSSpec{},
 	}
 
-	_, cm, err := desiredCABundleConfigMap(dns, true, &sourceConfigmap, destName)
-	if err != nil {
+	desired, cm, err := desiredCABundleConfigMap(dns, true, &sourceConfigmap, destName)
+	if err != nil || desired == false {
 		t.Errorf("Unexpected error : %v", err)
 	} else if diff := cmp.Diff(cm.Data, sourceConfigmap.Data); diff != "" {
 		t.Errorf("Unexpected CA Bundle ConfigMap data;\n%s", diff)
 	} else if diff := cmp.Diff(cm.OwnerReferences, []metav1.OwnerReference{dnsOwnerRef(dns)}); diff != "" {
 		t.Errorf("Unexpected CA Bundle ConfigMap OwnerReference;\n%s", diff)
+	}
+
+	desired, cm, err = desiredCABundleConfigMap(dns, false, &sourceConfigmap, destName)
+	if desired != false || cm != nil || err != nil {
+		t.Errorf("Unexpected error : %v", err)
+	}
+
+	var delTimestamp *metav1.Time
+	delTimestamp = &metav1.Time{
+		Time: time.Now(),
+	}
+	dns.DeletionTimestamp = delTimestamp
+
+	desired, cm, err = desiredCABundleConfigMap(dns, true, &sourceConfigmap, destName)
+	if desired != false || cm != nil || err != nil {
+		t.Errorf("Unexpected error : %v", err)
 	}
 }
